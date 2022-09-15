f72e824faec663647584d2cc87568eeaf99683b2
shih-ting-lin <107251983+shih-ting-lin@users.noreply.github.com>
2022-09-15 16:37:51 +0800

    Update Game1.kt

--- src/main/kotlin/com/tina/Game1.kt
+++ src/main/kotlin/com/tina/Game1.kt
@@ -1,2 +1,24 @@
 package com.tina
 
+import java.util.*
+
+
+fun main(args: Array<String>) {
+    val secret = Random().nextInt(10)+1
+    println(secret)
+    val scanner = Scanner(System.`in`)
+    var number = 0
+    while (number != secret) {
+        print("請輸入數字:")
+        //number = scanner.nextInt()
+        //可改成
+        number = readLine()!!.toInt()
+        if (number > secret) {
+            println("lower")
+        } else if (number < secret)
+            println("higher")
+        else {
+            println("bingo!!!")
+        }
+    }
+}
